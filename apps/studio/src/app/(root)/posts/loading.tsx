cade46db782b45aa8772a79f32d9840e1177cf7d
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-04-25 19:26:42 +0700

    feat(studio): 🧹 clean unused comment

--- apps/studio/src/app/(root)/posts/loading.tsx
+++ apps/studio/src/app/(root)/posts/loading.tsx
@@ -6,9 +6,6 @@ export default function Loading() {
     <ResizablePanel defaultSize={defaultLayout[2]}>
       <section>
         <ScrollArea className="h-[calc(83vh)]">
-          {/* <div className="flex w-max items-center justify-center">
-            <h1 className="text-center text-4xl font-bold">Loading...</h1>
-          </div> */}
           <SupermanLoader />
         </ScrollArea>
       </section>
