dbdc772003a31b39faef467559f76040272da19a
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-04-26 13:47:04 +0700

    feat(middleware): i dunno wtf is goin on

--- apps/studio/src/lib/config.ts
+++ apps/studio/src/lib/config.ts
@@ -1,8 +1,10 @@
+import { env } from "@packages/env";
+
 export const dev = process.env.NODE_ENV === "development";
 
 export const defaultLayout = [10, 25, 65];
 
 const url =
-  (process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000") + "/api";
+  ( env.NEXT_PUBLIC_STUDIO_APP_URL || "http://localhost:3000") + "/api";
 
 export const baseUrl = dev ? "http://localhost:3000/api" : url;
